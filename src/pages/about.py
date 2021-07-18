c0ba695a7af5c32163732d09117af6650f151b90
Rolando Trevino Lozano <57056839+rolando-trevino@users.noreply.github.com>
2021-07-18 04:17:40 -0500

    improved UI exp

--- src/pages/about.py
+++ src/pages/about.py
@@ -3,4 +3,13 @@ import streamlit as st
 
 
 def write():
+    if st.session_state['navigation_current'] == None:
+        st.session_state['navigation_current'] = "About"
+        st.session_state['navigation_changed'] = False
+    elif (st.session_state['navigation_current'] != "About"):
+        st.session_state['navigation_current'] = "About"
+        st.session_state['navigation_changed'] = True
+    elif (st.session_state['navigation_current'] == "About"):
+        st.session_state['navigation_changed'] = False
+
     st.write("# ML/DS Gallery - About")
